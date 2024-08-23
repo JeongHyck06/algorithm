60f5d4470f9020ed730489030c6e37c5d59c642e
Jack <147740943+JeongHyck06@users.noreply.github.com>
2024-08-23 16:25:36 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  algorithm
+//
+//  Created by 임정혁 on 8/23/24.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    double a,b;
+    cout<<fixed;
+    cout.precision(10);
+    cin >> a >> b;
+    cout << a/b << endl;
+}

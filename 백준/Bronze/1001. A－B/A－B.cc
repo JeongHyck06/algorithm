d024a63972a6f03c79639dbf432ec5519c8eb0d0
Jack <147740943+JeongHyck06@users.noreply.github.com>
2024-08-23 16:22:35 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cc
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  algorithm
+//
+//  Created by 임정혁 on 8/23/24.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    int a,b;
+    cin >> a >> b;
+    cout << a-b << endl;
+}

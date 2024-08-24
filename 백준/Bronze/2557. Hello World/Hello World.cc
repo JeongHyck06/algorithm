3cd03240be0be81ae85c43145043c09baeebbbe6
Jack <147740943+JeongHyck06@users.noreply.github.com>
2024-08-24 23:53:29 +0900

    [Bronze V] Title: Hello World, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.cc
@@ -0,0 +1,12 @@
+//
+//  main.cpp
+//  algorithm
+//
+//  Created by 임정혁 on 8/23/24.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    cout << "Hello World!" << endl;
+}

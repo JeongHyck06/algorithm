5d1aa729512d0418aa4f2ed6e3ab06fddde45ec8
Jack <147740943+JeongHyck06@users.noreply.github.com>
2024-08-25 22:04:52 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    int n;
+    cin >> n;
+
+    for(int i=1; i <n+1; i++){
+        cout << i << "\n";
+    }
+
+}
+

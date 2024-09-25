4eb002829f5dae1853cee29f3d6fccef13f86245
Jack <147740943+JeongHyck06@users.noreply.github.com>
2024-09-25 11:20:59 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    string str;
+    cin >> str;
+    cout << str.length();
+}

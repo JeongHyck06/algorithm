0aeaf3e60f49ebb089c4d1778f023c79c3e7ed2b
Jack <147740943+JeongHyck06@users.noreply.github.com>
2024-10-16 11:59:35 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.cc
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int input, output;
+    int now = 0;
+    int max = 0;
+    for (int i = 0; i < 10; i++) {
+        cin >> output;
+        now -= output;
+
+        cin >> input;
+        now += input;
+
+        if (max < now) {
+            max = now;
+        }
+    };
+
+    cout << max;
+}

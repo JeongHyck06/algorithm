1ecc7dc59c3f9a82b3dce5b63c47b7399ebbf649
Jack <147740943+JeongHyck06@users.noreply.github.com>
2024-10-19 23:06:58 +0900

    [Bronze II] Title: 농구 경기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/1159. 농구 경기/농구 경기.cc
@@ -0,0 +1,28 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    int cnt = 0;
+    cin >> n;
+    vector<string> name(n);
+    int count[26] = {0};
+
+    for (int i = 0; i < n; i++) {
+        cin >> name[i];
+        count[name[i][0] - 'a']++;
+    }
+
+    bool check = false;
+    for (int i = 0; i < 26; i++) {
+        if (count[i] >= 5) {
+            cout << char(i + 'a');
+            check = true;
+        }
+    }
+    if (!check) {
+        cout << "PREDAJA";
+    }
+}

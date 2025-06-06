d45aa4c031b7a05e95e0157c088f5b7a340a3756
임정혁 <147740943+JeongHyck06@users.noreply.github.com>
2025-06-06 23:23:25 +0900

    [Silver IV] Title: 30, Time: 148 ms, Memory: 16420 KB -BaekjoonHub

---
+++ 백준/Silver/10610. 30/30.java
@@ -0,0 +1,31 @@
+//
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String n = br.readLine();
+        char[] digits = n.toCharArray();
+
+        int sum = 0;
+        boolean hasZero = false;
+
+        for (char c : digits) {
+            int digit = c - '0';
+            sum += digit;
+            if (digit == 0) hasZero = true;
+        }
+
+        if (sum % 3 != 0 || !hasZero) {
+            System.out.println(-1);
+        } else {
+            Arrays.sort(digits);
+            StringBuilder sb = new StringBuilder(new String(digits));
+            System.out.println(sb.reverse().toString());
+        }
+    }
+}

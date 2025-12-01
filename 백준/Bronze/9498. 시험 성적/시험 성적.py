de0ae74fddd56101a21883464e58351d3dd68f3e
임정혁 <147740943+JeongHyck06@users.noreply.github.com>
2025-12-01 23:15:53 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,13 @@
+# 9498 - 시험 성적
+score = int(input())
+
+if 90 <= score <= 100:
+    print("A")
+elif 80 <= score:
+    print("B")
+elif 70 <= score:
+    print("C")
+elif 60 <= score:
+    print("D")
+else:
+    print("F")
